e0035f72de4c096d71f890d1fd1488ef27e6d190
McKenzie Joyce <mckenziejoyce6@gmail.com>
2019-10-29 23:54:58 -0400

    Added sidebar

--- managerui/src/SelectEmployees.js
+++ managerui/src/SelectEmployees.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme  } from '@material-ui/core/styles';
 import Logo from './whiteLogo.png'
 import ProPic from './propic.jpg'
 import { Button } from '@material-ui/core'
@@ -17,13 +17,25 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Avatar from '@material-ui/core/Avatar';
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import SearchIcon from '@material-ui/icons/Search';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ChatIcon from '@material-ui/icons/Chat';
+import HomeIcon from '@material-ui/icons/Home';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Drawer from '@material-ui/core/Drawer';
+import clsx from 'clsx';
 
+
+const drawerWidth = 240;
 
 const messages = [
   {
@@ -70,6 +82,30 @@ const messages = [
   },
 ];
 
+const icons = [
+  {
+    id:1,
+    primary: 'Home',
+    person: <HomeIcon />,
+  },
+  {
+    id:2,
+    primary: 'Chats',
+    person: <ChatIcon />,
+  },
+  {
+    id:3,
+    primary: 'Contact Support',
+    person: <ContactSupportIcon />
+  },
+  {
+    id:4,
+    primary: 'Settings',
+    person: <SettingsIcon />
+  },
+
+];
+
 const useStyles = makeStyles(theme => ({
   text: {
     padding: theme.spacing(2, 2, 0),
@@ -100,9 +136,22 @@ const useStyles = makeStyles(theme => ({
   appBar: {
     top: 'auto',
     bottom: 0,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   root: {
     flexGrow: 1,
+    display: 'flex',
   },
   //The + sign
   fabButton: {
@@ -113,36 +162,82 @@ const useStyles = makeStyles(theme => ({
     right: 0,
     margin: '0 auto',
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
 }));
 
 //https://material-ui.com/components/lists/
 export default function EmployeeSelection() {
   const classes = useStyles();
+  const theme = useTheme();
   const [checked, setChecked] = React.useState([1]);
+  const [open, setOpen] = React.useState(false);
 
   const handleToggle = value => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
-
     if (currentIndex === -1) {
       newChecked.push(value);
-    } else {
+    }
+    else {
       newChecked.splice(currentIndex, 1);
     }
-
     setChecked(newChecked);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
 
   return (
     <div className = "EmployeeSelection">
-    <AppBar position="static">
+    <AppBar position="static" className={clsx(classes.appBar, {[classes.appBarShift]: open,})}>
        <Toolbar className={classes.toolbar}>
          <IconButton
            edge="start"
-           className={classes.menuButton}
+           className={clsx(classes.menuButton, open && classes.hide)}
            color="inherit"
            aria-label="open drawer"
+           onClick={handleDrawerOpen}
          >
            <MenuIcon />
          </IconButton>
@@ -154,6 +249,35 @@ export default function EmployeeSelection() {
          </IconButton>
        </Toolbar>
      </AppBar>
+     <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+        {icons.map(({ id, primary, person }) => (
+          <React.Fragment key={id}>
+            <ListItem button>
+              <ListItemIcon>
+                {person}
+              </ListItemIcon>
+              <ListItemText primary={primary} />
+            </ListItem>
+          </React.Fragment>
+        ))}
+        </List>
+        <Divider />
+      </Drawer>
       <Paper square className={classes.paper}>
         <List className={classes.list}>
           {messages.map(({ id, primary, secondary, person, value }) => (
@@ -189,7 +313,7 @@ export default function EmployeeSelection() {
               <AddIcon />
             </Fab>
             <div className={classes.grow} />
-            <ListItemText> .</ListItemText>
+            <ListItemText>  .</ListItemText>
             <IconButton color="inherit">
               <AddIcon />
             </IconButton>

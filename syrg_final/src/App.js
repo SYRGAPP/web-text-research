304efb6784f5b1f9ade64b2d79db4c99e4882585
Taylor Rahman <ftrahman@bu.edu>
2019-11-01 10:37:37 -0400

    Completely redid the log-in and have the apolloL server being set up

---
+++ syrg_final/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import SignIn from './screens/SignIn.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <SignIn />
+      </header>
+    </div>
+  );
+}
+
+export default App;
